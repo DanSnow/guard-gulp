630690726fc89b08188591c8965a4a451a997835
DanSnow <dododavid006@gmail.com>
2015-01-28 21:32:16 +0800

    Remove output parse

--- lib/guard/gulp/runner.rb
+++ lib/guard/gulp/runner.rb
@@ -9,19 +9,7 @@ module Guard
       end
 
       def run_all
-        output, _ = Open3.capture2e("gulp --no-color #{@options[:task]}")
-        result = Formatter.new(output)
-        if result.fail_tasks.empty?
-          Compat::UI.notify("#{@options[:task]} done", title: "Gulp")
-          Compat::UI.info("#{@options[:task]} done")
-        else
-          result.fail_tasks.each {|task_name|
-            Compat::UI.notify("#{task_name} fail", title: "Gulp")
-            Compat::UI.error("#{task_name} fail")
-          }
-          Compat::UI.error("Backtrace:")
-          Compat::UI.error(result.backtrace)
-        end
+        `gulp --no-color #{@options[:task]}`
       end
     end
   end
